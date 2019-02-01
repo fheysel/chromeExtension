1cbaa12ca0f75fd06b454e54ebac7e3c97359381
heyselt <15tjh2@queensu.ca>
2019-02-01 16:31:42 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var slotsBooked = 0;
+
+function updateColour(slot){
+  slot.backgroundColor = "red";
+}
+
+var theParent = document.getElementById("theCalendar");
+theParent.addEventListener("click", clickReact, false);
+
+function clickReact(e){
+  if (e.target != e.currentTarget && e.target.className == 'slot') {
+    if(slotsBooked < 3){
+      slotsBooked++;
+      e.target.style.backgroundColor = "red";
+    }
+    else{
+      alert("D!bs only allows you to have 3 hours reserved at a time")
+    }
+
+    // alert("hello " + e.target.id + " poop " + e.target.className);
+  }
+  e.stopPropagation;
+}
